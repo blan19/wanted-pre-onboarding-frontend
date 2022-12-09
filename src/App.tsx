8675758b95e54a7d2b6b185c49f3a35b9489ec0b
박준서 <oponize@naver.com>
2022-12-09 16:05:05 +0900

    feat: Routes Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <h1></h1>
-    </div>
+    <Routes>
+      <Route path="/"></Route>
+      <Route path="/auth">
+        <Route path="signIn" />
+        <Route path="signUp" />
+      </Route>
+      <Route path="/todos">
+        <Route path=":todoId" />
+      </Route>
+    </Routes>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter(createRoutesFromElements(<App />));
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -9,7 +16,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
