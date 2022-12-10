1734fd0ed0e42b38d4ca27defb8c471dc346cdcf
Junseo <oponize@naver.com>
2022-12-10 21:00:18 +0900

    feat: 미디어 쿼리 추가

---
+++ src/constants/device.ts
@@ -0,0 +1,6 @@
+type Device = "mobile" | "tablet" | "desktop";
+
+const DEVICE_LIST: Device[] = ["mobile", "tablet", "desktop"];
+
+export type { Device };
+export default DEVICE_LIST;
---
+++ src/styles/mediaQuery.ts
@@ -0,0 +1,17 @@
+import { Device } from "../constants/device";
+
+type DeviceQuery = {
+  [key in Device]: string;
+};
+
+const deviceQuery: DeviceQuery = {
+  mobile: "(max-width: 703px)",
+  tablet: "(min-width: 704px) and (max-width: 895px)",
+  desktop: "(min-width: 896px) and (max-width: 1919px)",
+};
+
+const applyMediaQuery = (...deviceList: Device[]) =>
+  "@media screen and " +
+  deviceList.map((device) => deviceQuery[device]).join(",");
+
+export { deviceQuery, applyMediaQuery };
