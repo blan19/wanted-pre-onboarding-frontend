e3ba633e9ff06c09fce70c4eafe78b99674b619f
박준서 <oponize@naver.com>
2022-12-12 20:25:26 +0900

    feat: router 등록

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import Root from "./routes/Root";
+import SignIn from "./routes/auth/SignIn";
+import SignUp from "./routes/auth/SignUp";
+import Todo from "./routes/todo/Todo";
+import CreateTodo from "./routes/todo/CreateTodo";
 
 const App = (
   <Route path="/">
+    <Route index element={<Root />} />
     <Route path="auth">
-      <Route path="signIn" />
-      <Route path="signUp" />
+      <Route path="signin" element={<SignIn />} />
+      <Route path="signup" element={<SignUp />} />
     </Route>
-    <Route path="todos">
-      <Route path=":todoId" />
+    <Route path="todo" element={<Todo />}>
+      <Route path="create" element={<CreateTodo />} />
     </Route>
   </Route>
 );
