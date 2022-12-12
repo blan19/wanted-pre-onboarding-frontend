d9e77bdf6c34e2363a9370b18910ccb181562abe
Junseo <oponize@naver.com>
2022-12-12 11:41:57 +0900

    feat: form validator util 추가

---
+++ src/utils/validator.ts
@@ -0,0 +1,27 @@
+const fieldValidator = {
+  email: (value: string | undefined) => {
+    if (!value) return "필수로 입력해야합니다.";
+
+    const valueIsEmailPattern =
+      /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/.test(value);
+
+    if (!valueIsEmailPattern) {
+      return "이메일을 입력해주세요.";
+    }
+
+    return null;
+  },
+  password: (value: string | undefined) => {
+    if (!value) return "필수로 입력해야합니다.";
+
+    const valueIsLongEnough = value.length >= 8;
+
+    if (!valueIsLongEnough) {
+      return "8글자 이상이여야 합니다.";
+    }
+
+    return null;
+  },
+};
+
+export default fieldValidator;
