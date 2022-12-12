e9d094389efc107e325212ff9c9e90e1dcca3eb9
박준서 <oponize@naver.com>
2022-12-12 18:04:46 +0900

    feat: error util 추가

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -26,4 +26,29 @@ function debounce<Args extends unknown[]>(
   return debounced;
 }
 
-export { debounce };
+type ErrorWithMessage = {
+  message: string;
+};
+
+const isErrorWithMessage = (error: unknown): error is ErrorWithMessage => {
+  return (
+    typeof error === "object" &&
+    error !== null &&
+    "message" in error &&
+    typeof (error as Record<string, unknown>).message === "string"
+  );
+};
+
+const toErrorWithMessage = (maybeError: unknown): ErrorWithMessage => {
+  if (isErrorWithMessage(maybeError)) return maybeError;
+
+  try {
+    return new Error(JSON.stringify(maybeError));
+  } catch {
+    return new Error(String(maybeError));
+  }
+};
+
+const getErrorMessage = (error: unknown) => toErrorWithMessage(error).message;
+
+export { debounce, getErrorMessage };
