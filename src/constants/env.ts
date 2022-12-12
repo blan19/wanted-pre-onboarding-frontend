08e639c94e61c7542d657cdd52f6497d92171294
박준서 <oponize@naver.com>
2022-12-12 18:03:51 +0900

    feat: 환경변수 상수화

---
+++ src/constants/env.ts
@@ -0,0 +1,7 @@
+const API_BASE_URL =
+  process.env.REACT_APP_SERVER_URL ||
+  "https://pre-onboarding-selection-task.shop";
+
+const TOKEN = process.env.REACT_APP_TOKEN_KEY || "TOKEN";
+
+export { API_BASE_URL, TOKEN };
