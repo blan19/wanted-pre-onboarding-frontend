17467b7c33876f8cabc88c58ae7aef78869b27d5
Junseo <oponize@naver.com>
2022-12-13 01:40:36 +0900

    feat: todo api 로직

--- src/utils/api/todo.ts
+++ src/utils/api/todo.ts
@@ -1,6 +1,7 @@
 import { API_BASE_URL } from "../../constants/env";
 import { getErrorMessage } from "../misc";
 import { getToken } from "../token";
+import { FieldsValues } from "./auth";
 import fetcher from "./fetcher";
 
 const token = getToken();
@@ -21,4 +22,55 @@ const getTodos = async () => {
   }
 };
 
-export { getTodos };
+const createTodo = async (fieldsValues: FieldsValues) => {
+  try {
+    const response = await fetcher(`${API_BASE_URL}/todos`, {
+      method: "POST",
+      headers: {
+        Authorization: header,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(fieldsValues),
+    });
+
+    return response;
+  } catch (error) {
+    throw new Error(getErrorMessage(error));
+  }
+};
+
+const updateTodo = async (id: number, todo: string) => {
+  try {
+    const response = await fetcher(`${API_BASE_URL}/todos`, {
+      method: "PUT",
+      headers: {
+        Authorization: header,
+        "Content-Type": "application/json",
+      },
+      params: id,
+      body: JSON.stringify({ todo }),
+    });
+
+    return response;
+  } catch (error) {
+    throw new Error(getErrorMessage(error));
+  }
+};
+
+const deleteTodo = async (id: number) => {
+  try {
+    const response = await fetcher(`${API_BASE_URL}/todos`, {
+      method: "DELETE",
+      headers: {
+        Authorization: header,
+      },
+      params: id,
+    });
+
+    return response;
+  } catch (error) {
+    throw new Error(getErrorMessage(error));
+  }
+};
+
+export { getTodos, createTodo, updateTodo, deleteTodo };
